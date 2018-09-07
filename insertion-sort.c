9748918d71a0fc73034909fa3e9fbdb28f18de21
Saurav Suman <90.suman@gmail.com>
2018-09-07 11:50:03 +0530

    Insertion Sort for numbers

---
+++ insertion-sort.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+/*Start from index 1, left side is already sorted, pick a element from right array sequentially,
+compare with sorted list to find correction location for insertion*/
+void insertionSort(int arr[], int size)
+{
+	int i = 1,j = 0;
+	for (;i<size;i++)
+	{
+		j = i-1;
+		int temp = arr[i];
+		while(j > -1 && arr[j] > temp)
+		{
+			arr[j+1] = arr[j];
+			j--;
+		}
+		arr[j+1] = temp;
+	}
+}
+
+int main (void)
+{
+	int arr[] = {9,8,7,6,5,4,3,2,1,0};
+	int size = sizeof(arr)/sizeof(arr[0]);
+	printf("Before Sorting\n");
+	for (int i = 0;i< size;i++)
+		printf("%d ",arr[i]);
+
+	printf("\nAfter Sorting\n");
+	insertionSort(arr,size);
+	for(int i = 0;i<size;i++)
+		printf("%d ",arr[i]);
+
+	printf("\n");
+	return 0;
+}
+		
+
+	
